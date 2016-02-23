c62437dfd6c7d7a74ec510bbc927e5677a7a7921
FriedRice <n.mclaughlin21@gmail.com>
2016-02-23 04:38:40 -0500

    Made jumping smoother.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,19 +6,24 @@ public class Player : MonoBehaviour {
     const KeyCode MOVE_DOWN_KEY = KeyCode.S;
     const KeyCode MOVE_RIGHT_KEY = KeyCode.D;
     const KeyCode JUMP_KEY = KeyCode.Space;
+	const KeyCode kMicrosoftAButton = KeyCode.Joystick1Button0;
 
-	public Rigidbody2D			rigid;
-	public bool					inAir;
-	public float				speed = 4.0f;
+	public Rigidbody2D	rigid;
+	public bool	inAir;
+	public float move_speed = 4.0f;
+    public float jump_scale_factor = 3.0f;
 	public int player = Controls.player1;
 	public bool useController;
-	const KeyCode kMicrosoftAButton = KeyCode.Joystick1Button0;
 	public bool has_triggered = false;
-	public bool right_trigger_down = false;
+    public bool right_trigger_down = false;
+
+    float jump_speed;
+
 	// Use this for initialization
 	void Start () {
 		rigid = GetComponent<Rigidbody2D>();
 		inAir = false;
+        jump_speed = rigid.gravityScale * jump_scale_factor;
 	}
 	
 	void Update () {
@@ -33,48 +38,41 @@ public class Player : MonoBehaviour {
 	}
 
 	void updateMovement() {
-		Vector3 vel_vec = Vector3.zero;
+        float x_direction = 0f;
 		if (useController) {
-			vel_vec.x = speed * Input.GetAxis (Controls.axes_codes [player, Controls.axis_left_joy_hor]);
-		} 
-		else {
-			bool w = false;
+			x_direction = move_speed * Input.GetAxis(Controls.axes_codes [player, Controls.axis_left_joy_hor]);
+		} else {
 			bool a = Input.GetKey (MOVE_LEFT_KEY);
-			bool s = Input.GetKey (MOVE_DOWN_KEY);
 			bool d = Input.GetKey (MOVE_RIGHT_KEY);
-
-			vel_vec.x = a ? -1 : (d ? 1 : 0);
-			vel_vec.y = s ? -1 : (w ? 1 : 0);
-
-			vel_vec.Normalize ();
-			vel_vec *= speed;
+			x_direction = a ? -1 : (d ? 1 : 0);
 		}
-		rigid.velocity = vel_vec;
+		rigid.velocity = new Vector2(x_direction * move_speed, rigid.velocity.y);
 	}
 
     void updateJump() {
-		if (Input.GetAxis (Controls.axes_codes [player, Controls.axis_right_trigger]) > 0.0f) {
+		if (Input.GetAxis (Controls.axes_codes[player, Controls.axis_right_trigger]) > 0.0f) {
 			right_trigger_down = true;
 		}
+
 		if (useController) {
 			if (!has_triggered && right_trigger_down && (!inAir)) {
-				has_triggered = true;
-				transform.Translate (new Vector3 (0, 1));
-				inAir = true;
-				print ("jump!");
+                has_triggered = true;
+                jump();
 			} else if (has_triggered && right_trigger_down && (!inAir)) {
-				transform.Translate (new Vector3 (0, 1));
-				inAir = true;
-				print ("jump!");
+                jump();
 			}
-		}
-		else {
+		} else {
 			if (Input.GetKeyDown (JUMP_KEY) && (!inAir)) {
-				transform.Translate (new Vector3 (0, 1));
-				inAir = true;
-				print ("jump!");
+                jump();
 			}
 		}
+    }
+
+    void jump() {
+        //transform.Translate(new Vector3 (0, 1));
+        rigid.velocity = new Vector2(rigid.velocity.x, rigid.velocity.y + jump_speed);
+        inAir = true;
+        print ("jump!");
     }
 
 	void OnCollisionEnter2D(Collision2D other) {
