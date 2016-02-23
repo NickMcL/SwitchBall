5ba4dc7ec249c9e4673b2c1a109f4167fbf330cc
FriedRice <n.mclaughlin21@gmail.com>
2016-02-23 06:57:16 -0500

    Set up swap attack to use team swap.

--- Assets/Scripts/swapAttack.cs
+++ Assets/Scripts/swapAttack.cs
@@ -6,18 +6,13 @@ public class swapAttack : MonoBehaviour {
     public Vector2 movement_vector;
     public int source_player_id;
     public GameManager.TeamType bullet_team;
-    public float rotations_per_minute;
-
-//	public PlayerManager _fromPlayer;
-//	private PlayerManager _toPlayer;
-//	private bool changed;
+    public float rotations_per_minute = 20f;
 
     Rigidbody2D rb;
 
     // Use this for initialization
     void Start() {
         rb = this.GetComponent<Rigidbody2D>();
-        rotations_per_minute = 20f;
     }
 
     // Update is called once per frame
@@ -32,54 +27,8 @@ public class swapAttack : MonoBehaviour {
         }
         if (coll.gameObject.tag == "Player" &&
                 coll.gameObject.GetComponent<PlayerManager>().player_id != source_player_id) {
-            print("Hit player");
+            print("Hit player " + coll.gameObject.GetComponent<PlayerManager>().player_id);
+            // coll.gameObject.GetComponent<PlayerManager>().swapTeam();
         }
     }
-
-
-//		if (coll.gameObject.tag == "Player"&&!changed) {
-//			_toPlayer = coll.gameObject.GetComponent<PlayerManager> ();
-//			//hit itself return nothing
-//			if (_toPlayer == _fromPlayer)
-//				return;
-//			changed = true; 
-//			//if hit enemy
-//			if (_toPlayer.Team != _fromPlayer.Team) {
-//				//when it's 1v1v1v1 changes it to 1v3
-//				if (GameManager.Instance.Mode == GameManager.gameType.FFA) {
-//					GameManager.Instance.changeToOvT (_toPlayer);
-//				} 
-//				else {
-//					_toPlayer.ChangeTeam (_fromPlayer.Team);
-//					if(GameManager.Instance.Mode==GameManager.gameType.TvT){
-//						
-//						GameManager.Instance.Mode = GameManager.gameType.OvT;
-//					}if (GameManager.Instance.Mode == GameManager.gameType.OvT) {
-//						
-//						//checkif there are 4 team
-//						if(!GameManager.Instance.returnToFFA()){
-//							GameManager.Instance.Mode = GameManager.gameType.TvT;
-//						}
-//					}
-//				}
-//			}
-//			//if hit teammate
-//			else {
-//				//make teammate to enemy
-//				if (_toPlayer.Team == GameManager.TeamType.A) 
-//					_toPlayer.ChangeTeam (GameManager.TeamType.B);
-//				
-//
-//				else _toPlayer.Team = GameManager.TeamType.A;
-//
-//				//change the gamemode
-//				if (GameManager.Instance.Mode == GameManager.gameType.TvT) {
-//					GameManager.Instance.Mode = GameManager.gameType.OvT;
-//				} else GameManager.Instance.Mode = GameManager.gameType.TvT;
-//			}
-//
-//			Destroy (this.gameObject);
-//		}
-
-
 }
