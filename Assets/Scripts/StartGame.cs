2c031f4775151772db5cfcdd6e3db90ee0962bc5
unknown <delamaan@umich.edu>
2016-03-09 19:09:06 -0500

    main menu changes

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartGame: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene("Scene_Main");
+        }
+    }
+}
